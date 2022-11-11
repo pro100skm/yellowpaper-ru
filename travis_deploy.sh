2cad2b6800d8f2a6558be686a414c930d75922f4
Mr.Popovitsch <pro100skm@yandex.ru>
2022-11-11 11:52:48 +0300

    feat: started to translate

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -9,8 +9,8 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 git checkout --orphan gh-pages
 git rm --cached -r .
 echo "# Automatic build" > README.md
-echo "Built pdf from \`$SHA\`. See https://github.com/ethereum/yellowpaper/ for details." >> README.md
-echo "The generated pdf is here: https://ethereum.github.io/yellowpaper/paper.pdf" >> README.md
+echo "Built pdf from \`$SHA\`. See https://github.com/pro100skm/yellowpaper-ru/ for details." >> README.md
+echo "The generated pdf is here: https://pro100skm.github.io/yellowpaper-ru/paper.pdf" >> README.md
 echo '<html><head><meta http-equiv="refresh" content="0; url=paper.pdf" /></head><body></body></html>' > index.html
 mv build/Paper.pdf paper.pdf
 git add -f README.md index.html paper.pdf
