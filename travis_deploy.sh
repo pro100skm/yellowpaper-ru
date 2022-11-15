e45e8a818894dee99de124b5361a214b1e838f79
Mr.Popovitsch <pro100skm@yandex.ru>
2022-11-15 18:00:43 +0300

    feat: hardcoded

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -16,8 +16,8 @@ mv build/Paper.pdf paper.pdf
 git add -f README.md index.html paper.pdf
 git commit -m "Built pdf from {$SHA}."
 
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY_VAR="encrypted_19a81de38b62_key"
+ENCRYPTED_IV_VAR="encrypted_19a81de38b62_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploykey.enc -out deploykey -d
